c0dd3ab3f867cde287bd837490ca60ea06110285
pedritoeldulce <franceskolyperez@gmail.com>
2023-05-28 22:24:55 -0500

    pre-commit XD

---
+++ README.md
@@ -0,0 +1,16 @@
+Instalar Linters y Formatters
+### Linters
+
+- flake8
+- pylint
+-
+## Formatters
+
+- Black
+- Isort
+
+
+- Vamos a instalar los paquetes dentro de requeriments.txt
+
+## Pre-commit
+Los hooks de Git son útiles para identificar problemas simples antes de enviarlos a revisión de código
---
+++ main.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class SwapTestSuite(unittest.TestCase):
+    """
+    Swap Operation Test Case
+    """
+
+    def setUp(self):
+        self.a = 1
+        self.b = 2
+
+
+class OddOrEvenTestSuite(unittest.TestCase):
+    """
+    This is the Odd or Even Test case Suite
+    """
+
+    def setUp(self):
+        self.value1 = 1
+        self.value2 = 2
+
+    def test_odd_even_operations(self):
+        pass
